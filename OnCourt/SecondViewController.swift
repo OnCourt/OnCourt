beea6d66ceaa37ff88e26267d43975ced8674c10
Ian Anderson <ianskiracer303@gmail.com>
2018-01-17 09:19:36 -0600

    Initial Commit

---
+++ OnCourt/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OnCourt
+//
+//  Created by Ian Anderson on 1/17/18.
+//  Copyright © 2018 Ian Anderson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
